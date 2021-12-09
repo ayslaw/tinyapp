5ef7b80a1e1511ae2c572065ae8f65e9a7060bc0
ayslaw <ayoubf.chp@gmail.com>
2021-12-09 03:56:37 -0500

    delete button added, and nodemon to package

--- express_server.js
+++ express_server.js
@@ -2,29 +2,53 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here?*/ req.params.longURL };
-  res.render("urls_show", templateVars);
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const randomStr = generateRandomString();
+  urlDatabase[randomStr] = req.body.longURL; // Pushes new tiny URL to urlDatabase
+  res.redirect(`/urls/${randomStr}`); // Redirects to newly made hyperlinked page
+  res.redirect(`/urls/${randomStr}`); // Redirects to tinyURL page
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-}); 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  delete urlDatabase[req.params.shortURL], req.params.shortURL; // Deletes URL entry
+  res.redirect("/urls"); // Redirects to main urls_index page
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
